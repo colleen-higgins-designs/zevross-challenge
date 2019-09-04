87e545210affcdeca0b417587f80e24da7ad5a2c
Colleen Higgins <cjh323@cornell.edu>
2019-09-04 10:58:35 -0400

    page displays unmarked map of contiguous US

---
+++ index.js
@@ -0,0 +1,7 @@
+// g
+// get wind turbines within contiguous us
+
+// get list of states or map through locations/turbines array and add a marker for each state with turbine (if not already added)
+
+// add marker
+// var marker = new mapboxgl.Marker().setLngLat([30.5, 50.5]).addTo(map);
